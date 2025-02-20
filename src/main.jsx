135379b02b865b4c90d78805c162ca452125b1c2
Khan Tufailahmed <tufailahmedkhan708@gmail.com>
2025-02-20 22:23:12 +0530

    check

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode, useState } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Home from "./pages/Home.jsx";
 import {Provider} from 'react-redux'
 import store from "./redux/store.js";
-import Shop from "./pages/Shop.jsx";
-import Contact from "./pages/Contact.jsx";
-import About from "./pages/About.jsx";
-import Cart from "./pages/cart.jsx";
-import Checkout from "./pages/Checkout.jsx";
-import Order from "./pages/Order.jsx";
+// import Shop from "./pages/Shop.jsx";
+// import Contact from "./pages/Contact.jsx";
+// import About from "./pages/About.jsx";
+// import Cart from "./pages/cart.jsx";
+// import Checkout from "./pages/Checkout.jsx";
+// import Order from "./pages/Order.jsx";
 
 
 createRoot(document.getElementById("root")).render(
